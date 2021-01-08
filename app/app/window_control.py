48f4792d74de501d219f7b87a43de04c5a1bb695
Samuli Kohomäki <samuli.kohomaki@gmail.com>
2021-01-09 00:41:31 +0200

    Added functionality to send keys to some application window

---
+++ app/app/window_control.py
@@ -0,0 +1,41 @@
+import logging
+
+from ahk import AHK
+
+
+class WindowControl:
+    def __init__(self, window_title: str) -> "WindowControl":
+        self.autohotkey = AHK()
+        self.window = self.autohotkey.find_window(title=window_title.encode("utf-8"))
+
+    def send_key(self, command: str, window_needs_clicking: bool = False) -> None:
+        """
+        Send given command (keys)
+
+        :param command: keys to send
+        :param window_needs_clicking: click the window after activating it and before sending keys
+        """
+        if self.window:
+            self.window.activate()
+            if window_needs_clicking is True:
+                self._click_window_center()
+            self._send_key(command)
+            logging.warning(f"Sent {command} to window {self.window.title}")
+
+    def _click_window_center(self) -> None:
+        """
+        Click the center of the window.
+        """
+        x, y, width, height = self.window.rect
+        click_x, click_y = x + width / 2, y + height / 2
+        logging.warning(
+            f"Window {self.window.title} requires clicking it before sending keys. "
+            f"Moving mouse and clicking to {click_x}, {click_y}"
+        )
+        self.autohotkey.click(click_x, click_y)
+
+    def _send_key(self, command) -> None:
+        """
+        Send given keys to window.
+        """
+        self.window.send(command, blocking=True)
