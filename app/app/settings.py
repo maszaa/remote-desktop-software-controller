50cb5b81db3f10fc074b5b3a0b26f8a6d4aa09f5
Samuli Kohomäki <samuli.kohomaki@gmail.com>
2021-01-09 01:19:38 +0200

    Allow LAN access

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import socket
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = "developmentsecretkey"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [socket.gethostbyname(socket.gethostname()), "127.0.0.1", "localhost"]
 
 
 # Application definition
