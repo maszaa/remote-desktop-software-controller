d02b0373d53e5686b20a74a51a4f0a7a40fa19a7
Samuli Kohomäki <samuli.kohomaki@gmail.com>
2021-01-19 09:37:20 +0200

    Screenshot: don't call SetForegroundWindow
    - it seems to cause problems if some Windows GUI elements are visible and over windows

--- app/app/screenshot.py
+++ app/app/screenshot.py
@@ -24,7 +24,6 @@ class Screenshot:
         hwnd = win32gui.FindWindow(None, self.window_title)
 
         if hwnd:
-            win32gui.SetForegroundWindow(hwnd)
             x, y, x1, y1 = win32gui.GetClientRect(hwnd)
             x, y = win32gui.ClientToScreen(hwnd, (x, y))
             x1, y1 = win32gui.ClientToScreen(hwnd, (x1 - x, y1 - y))
